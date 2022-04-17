cc2fcee920b6e5fca4b53142d17555837c131e29
unknown <oluwatobiloba8624@gmail.com>
2022-04-17 16:50:29 +0100

    added detector for multiple faces

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,8 +7,11 @@ const ImageLinkForm = (props) => {
     return (
         <div className='w-100 mt3'>
             <form id='form' className='center mt4 br2' onSubmit={props.onButtonSubmit}>
-                <input type='text' placeholder='Paste image url' onChange ={props.handleInputChange} value={props.url} className='urlBox pa2 ba br2 w-70 mr2 bg-transparent h2' style={{fontSize:'12px'}}/>
-                <button className='detect pa2 ba br2 w-30 link grow '>Detect face</button>
+                <input type='text' placeholder='Paste image url to detect faces' 
+                onChange ={props.handleInputChange} value={props.url} 
+                className='urlBox pa2 ba br2 w-100 mr2 bg-transparent h2' 
+                style={{fontSize:'12px'}}/>
+                
             </form>
         </div>
     )
