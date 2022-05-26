82a37ec8e2da290a94435f609bee9af7c4e97e18
unknown <oluwatobiloba8624@gmail.com>
2022-05-26 14:14:40 +0100

    error display

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,115 +1,140 @@
-import React from 'react';
-import 'tachyons';
+import React from "react";
+import "tachyons";
 
 class Register extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            registerSuccess: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      registerSuccess: "",
+    };
+  }
+
+  onNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  onEmailChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  onPasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  reloadRegister = () => {
+    this.setState({ registerSuccess: "" });
+  };
+
+  onSubmitRegister = (e) => {
+    e.preventDefault();
+
+    fetch("https://serene-beyond-02376.herokuapp.com/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((result) => {
+        if (result === "successful") {
+          this.setState({ registerSuccess: result });
+          setTimeout(() => {
+            this.props.onRouteChange("sign in");
+          }, 1500);
+        } else {
+          this.setState({ registerSuccess: result });
+          setTimeout(() => {
+            this.setState({ registerSuccess: null });
+          }, 3000);
         }
-    }
+      });
+  };
 
-    onNameChange = (e) => {
-        this.setState({name: e.target.value});
-    }
+  render() {
+    const message =
+      this.state.registerSuccess === "successful" ? (
+        <span className="green center"> Successful... </span>
+      ) : (
+        <span className="red center"> {this.state.registerSuccess} </span>
+      );
 
-    onEmailChange = (e) => {
-        this.setState({email: e.target.value});
-    }
+    return (
+      <article className="pa4 black-80 center pt6">
+        <form
+          action="sign-up_submit"
+          method="get"
+          acceptCharset="utf-8"
+          className="shadow-4 pa4"
+        >
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="ph0 mh0 fw6 f3 center">Sign Up</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Name
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="Name"
+                onChange={this.onNameChange}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email address
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={this.onEmailChange}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+                onChange={this.onPasswordChange}
+              />
+            </div>
+          </fieldset>
 
-    onPasswordChange = (e) => {
-        this.setState({password: e.target.value})
-    }
+          {message}
 
-    reloadRegister = () => {
-        this.setState({registerSuccess: ''})
-    }
-
-    onSubmitRegister = (e) => {
-        e.preventDefault();    
-        
-        fetch('https://serene-beyond-02376.herokuapp.com/register', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                name: this.state.name,
-                email: this.state.email,
-                password: this.state.password
-            })
-        })
-        .then(resp => resp.json())
-        .then(result => {
-                if(result === 'successful') {
-                    this.setState({registerSuccess: result})
-                    setTimeout(() => 
-                    {
-                        this.props.onRouteChange('sign in')
-                    }, 1500)
-                }else{
-                    this.setState({registerSuccess: result})
-                }
-        })
-    }
-
-    render() {
-        const message = (this.state.registerSuccess === 'successful') 
-        ? <span className='green center'> Successful... </span> 
-        : <span className='red center'> {this.state.registerSuccess} </span>;
-
-        return (
-            <article className="pa4 black-80 center pt6">
-                <form action="sign-up_submit" method="get" acceptCharset="utf-8" className='shadow-4 pa4'>
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="ph0 mh0 fw6 f3 center">Sign Up</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
-                            <input
-                             className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                             type="text" name="name"  
-                             id="Name"
-                             onChange={this.onNameChange}
-                            />
-                        </div>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email address</label>
-                            <input
-                             className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                             type="email" 
-                             name="email-address"  
-                             id="email-address"
-                             onChange={this.onEmailChange}
-                            />
-                        </div>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input
-                             className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                             type="password" 
-                             name="password"  
-                             id="password" 
-                             onChange={this.onPasswordChange}
-                            />
-                        </div>
-                        
-                    </fieldset>
-                    {message}
-                    <div className="mt3 center">
-                        <input
-                         className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" 
-                         onClick={this.onSubmitRegister} 
-                         type="submit" 
-                         value="Register" />
-                    </div>
-                    <div className="lh-copy mt3 center">
-                        <a href="#0" className="f7 link dim black db" onClick={() => this.props.onRouteChange('sign in')}>Sign in</a>
-                    </div>
-                </form>
-            </article>
-        )
-    }
-};  
+          <div className="mt3 center">
+            <input
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6"
+              onClick={this.onSubmitRegister}
+              type="submit"
+              value="Register"
+            />
+          </div>
+          <div className="lh-copy mt3 center">
+            <a
+              href="#0"
+              className="f7 link dim black db"
+              onClick={() => this.props.onRouteChange("sign in")}
+            >
+              Sign in
+            </a>
+          </div>
+        </form>
+      </article>
+    );
+  }
+}
 
 export default Register;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,105 +1,119 @@
-import React from 'react';
-import 'tachyons';
+import React from "react";
+import "tachyons";
 
 class SignIn extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            signInEmail : '',
-            signInPassword: '',
-            singInSuccess: '',
-            errorMessage: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: "",
+      signInPassword: "",
+      singInSuccess: "",
+      errorMessage: "",
+    };
+
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  onEmailChange = (e) => {
+    this.setState({ signInEmail: e.target.value });
+  };
+
+  onPasswordChange = (e) => {
+    this.setState({ signInPassword: e.target.value });
+  };
+
+  reloadSignIn = () => {
+    //TRYING TO REMEMBER WHY I DID THIS
+    this.setState({ singInSuccess: "" });
+  };
+
+  onSubmitSignIn = (e) => {
+    e.preventDefault();
+
+    fetch("https://serene-beyond-02376.herokuapp.com/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data.signInMessage === "success") {
+          this.setState({ singInSuccess: true });
+          this.props.getUser(data.user);
+          this.props.onRouteChange("home");
+        } else {
+          this.setState({ singInSuccess: "noSuccess" });
+          this.setState({ errorMessage: data });
+          setTimeout(() => {
+            this.setState({errorMessage: null});
+          }, 3000);
         }
+      })
+      .catch((err) => {
+        console.log("error fetching data");
+      });
+  };
 
-        this.onEmailChange = this.onEmailChange.bind(this);
-        this.onPasswordChange = this.onPasswordChange.bind(this);
-    }
-
-    onEmailChange = (e) => {
-        this.setState({signInEmail: e.target.value});
-    }
-
-    onPasswordChange = (e) => {
-        this.setState({signInPassword: e.target.value})
-    }
-
-    reloadSignIn = () => { //TRYING TO REMEMBER WHY I DID THIS
-        this.setState({singInSuccess: ''})
-    }
-
-    onSubmitSignIn = (e) => {
-        e.preventDefault();
-
-        fetch('https://serene-beyond-02376.herokuapp.com/signin', {
-            method: 'post',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                email: this.state.signInEmail,
-                password: this.state.signInPassword
-            })
-        })
-        .then(resp => resp.json())
-        .then(data => {
-            if (data.signInMessage === 'success') {
-                this.setState({singInSuccess: true})
-                this.props.getUser(data.user)
-                this.props.onRouteChange('home');
-            }else{
-                this.setState({singInSuccess: 'noSuccess'})
-                this.setState({errorMessage: data})
-            }
-        })
-        .catch(err => {
-            console.log("error fetching data")
-        })        
-    }
-
-    render () {
-        const {errorMessage} = this.state;
-        return (
-            <main className="pa4 black-80 center pt6 measure" >
-                <form className="measure shadow-4 pa4 " onLoad={this.reloadSignIn}>
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f3 fw6 ph0 mh0 center">Sign In</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input
-                             className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                             type="email" 
-                             name="email-address"  
-                             id="email-address" 
-                             onChange={this.onEmailChange}
-                            />
-                        </div>
-                        <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input
-                             className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                             type="password" 
-                             name="password"  
-                             id="password"
-                             onChange={this.onPasswordChange}
-                            />
-                            
-                        </div>
-                        {this.state.singInSuccess === 'noSuccess' && <span className='red f7 center'>{errorMessage}</span>}
-    
-                    </fieldset>
-                    <div className="center">
-                        <input
-                         className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f7 dib" 
-                         onClick={this.onSubmitSignIn} 
-                         type="submit" 
-                         value="Sign in" />
-                    </div>
-                    <div className="lh-copy mt3 center">
-                        <a href="#0" className="f7 link dim black db" onClick={() => this.props.onRouteChange('register')}>Register</a>
-                    </div>
-                </form>
-                
-            </main>
-        )
-    }
-};  
+  render() {
+    const { errorMessage } = this.state;
+    return (
+      <main className="pa4 black-80 center pt6 measure">
+        <form className="measure shadow-4 pa4 " onLoad={this.reloadSignIn}>
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f3 fw6 ph0 mh0 center">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={this.onEmailChange}
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+                onChange={this.onPasswordChange}
+              />
+            </div>
+            {this.state.singInSuccess === "noSuccess" && (
+              <span className="red f7 center">{errorMessage}</span>
+            )}
+          </fieldset>
+          <div className="center">
+            <input
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f7 dib"
+              onClick={this.onSubmitSignIn}
+              type="submit"
+              value="Sign in"
+            />
+          </div>
+          <div className="lh-copy mt3 center">
+            <a
+              href="#0"
+              className="f7 link dim black db"
+              onClick={() => this.props.onRouteChange("register")}
+            >
+              Register
+            </a>
+          </div>
+        </form>
+      </main>
+    );
+  }
+}
 
 export default SignIn;
